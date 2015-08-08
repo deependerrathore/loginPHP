7166fad6148dabff1eb3def348b79dd3e861d210
depeenderrathore <deepenrathore28@gmail.com>
2015-08-09 03:16:31 +0530

    Adding the registration fomr register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<form action="" method="POST">
+	<div class="field">
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username" value="" autocomplete="off">	
+	</div>
+	<div class="field">
+		<label for="password">Choose a password</label>
+		<input type="password" name="password" id="password">
+	</div>
+	<div class="field">
+		<label for="password again">Enter your password again</label>
+		<input type="password" name="password_again" id="password_again">
+	</div>
+	<div class="field">
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name">	
+	</div>
+	<input type="submit" value="Register">
+</form>
