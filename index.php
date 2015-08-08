41a46afdffea5f932dbc8fcc9a33d51df209dcf3
depeenderrathore <deepenrathore28@gmail.com>
2015-08-09 02:23:33 +0530

    Outputting all the users

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 require_once 'core/init.php';
 
-//$user = DB::getInstance()->query("select * from users");
-$user = DB::getInstance()->get('users',array('username','=','deepak'));
+$user = DB::getInstance()->query("select * from users");
+
 
 if(!$user->count()){
 	echo 'No user';
