855d6d2fc86c5ce19aa8de7352ee9ea815e4fa5a
depeenderrathore <deepenrathore28@gmail.com>
2015-08-09 02:24:38 +0530

    Outputting only one user

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php 
 require_once 'core/init.php';
 
-$user = DB::getInstance()->query("select * from users");
-
+//$user = DB::getInstance()->query("select * from users");
+$user = DB::getInstance()->get('users',array('username','=','deepak'));
 
 if(!$user->count()){
 	echo 'No user';
 }else{
-	foreach ($user->results() as $user) {
-		echo $user->username,'<br>';
-	}
+	echo $user->results()[0]->username;
 }
