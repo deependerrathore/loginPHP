f68c91aea1c6e4888521bf6316900db2599f70b8
depeenderrathore <deepenrathore28@gmail.com>
2015-08-09 17:55:29 +0530

    Login form is created

---
+++ login.php
@@ -0,0 +1,13 @@
+<form action="" method="POST">
+	<div class="field">
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username" autocomplete="off">
+	</div>
+	<div class="field">
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" autocomplete="off">
+	</div>
+
+	<input type="hidden" name="token" value=" <?php echo Token::generate(); ?>">
+	<input type="submit" value="Log in">
+</form>
