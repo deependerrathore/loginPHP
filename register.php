72f15993698c52ced401f6f36d3d09b3ad54bd81
depeenderrathore <deepenrathore28@gmail.com>
2015-08-09 17:31:38 +0530

    Registring the new user and also Implimented the Hash Class

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php 
+date_default_timezone_set('Asia/Calcutta');
 require_once 'core/init.php';
 
 if(Input::exists()){
@@ -29,8 +30,27 @@ if(Input::exists()){
 			));
 
 		if($validation->passed()){
-			Session::flash('success','You registered successfully!');
-			header('Location: index.php');
+			$user = new User();
+
+			$salt = Hash::salt(32);
+			
+			try{
+
+				$user->create(array(
+					'username' =>Input::get('username'),
+					'password' => Hash::make(Input::get('password'),$salt),
+					'salt' =>$salt,
+					'name' =>Input::get('name'),
+					'joined' => date('Y-m-d H:i:s'),
+					'group' => 1
+
+					));
+				Session::flash('home','You have been registered and can now login!');
+				header('Location: index.php');
+
+			}catch(Expception $e){
+				die($e->getMessage());
+			}
 		}else{
 			foreach ($validation->errors() as $error) {
 				echo $error, '<br>';
