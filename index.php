966208c35b07204064751db754d9c715fd6d549b
depeenderrathore <deepenrathore28@gmail.com>
2015-08-09 16:58:57 +0530

    flashing the message when user registered first time

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
 require_once 'core/init.php';
 
-$user = DB::getInstance()->update('users',3,array(
-	'password' => 'new password',
-	'salt' =>'new salt'
-	));
-
-//This will successfully insert the new record
+if(Session::exists('success')){
+	echo Session::flash('success');
+}
